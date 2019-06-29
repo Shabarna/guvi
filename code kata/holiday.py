7ec0e60c2bb1f448e5480f943451ed5dae1206ee
Shabarna <51675908+Shabarna@users.noreply.github.com>
2019-06-29 19:09:26 +0530

    Create holiday.py

---
+++ code kata/holiday.py
@@ -0,0 +1,12 @@
+a = 'Monday'
+b = 'Tuesday'
+c = 'Wednesday'
+d = 'Thursday'
+e = 'Friday'
+f = 'Saturday'
+g = 'Sunday'
+i = str(input())
+if(i==a or i==b or i==c or i==d or i==e):
+  print("no")
+else:
+  print("yes")
