97ab90d510291cbb0ee6b969337eb7bb520b3b6b
Shabarna <51675908+Shabarna@users.noreply.github.com>
2019-06-29 23:46:03 +0530

    Update array.py

--- code kata/array.py
+++ code kata/array.py
@@ -1,16 +1,7 @@
-n1,n2=input().split()
-a=input()
-n1=int(n)
-n2=int(k)
-a1=list(map(int,input().split()))
-a2=list(map(int,input().split()))
-a3=[]
-for i in range(n2):
-    a1.append(a2[i])
-    a3.append(max(l1))
-if(len(a3)==1):
-    print(a3[0])
-else:
-    for i in range(n2-1):
-        print(l3[i],end=' ')
-print(a3[n2-1])
+n1,n2=map(int,input().split())
+input()
+n3=list(map(int,input().split()))
+n4=list(map(int,input().split()))
+for x in n4:
+    n3.append(x)
+print(max(n3),end=' ')
