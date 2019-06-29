a07903d70aa6b5330f86147cd8e4ff5432ec434f
Shabarna <51675908+Shabarna@users.noreply.github.com>
2019-06-29 22:50:06 +0530

    Create sameline.py

---
+++ code kata/sameline.py
@@ -0,0 +1,8 @@
+
+n1=input().split()
+n2=input().split()
+n3=input().split()
+if(n1[0]==n2[0]==n3[0] or n1[1]==n2[1]==n3[1] or (n1[0]==n1[1] and n2[0]==n2[1] and n3[0]==n3[1])):
+    print('yes')
+else:
+print('no')
