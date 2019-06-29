dc3ec51261dc915edfebb13976d7e8cd48581bd9
Shabarna <51675908+Shabarna@users.noreply.github.com>
2019-06-29 23:37:29 +0530

    Create array.py

---
+++ code kata/array.py
@@ -0,0 +1,16 @@
+n1,n2=input().split()
+a=input()
+n1=int(n)
+n2=int(k)
+a1=list(map(int,input().split()))
+a2=list(map(int,input().split()))
+a3=[]
+for i in range(n2):
+    a1.append(a2[i])
+    a3.append(max(l1))
+if(len(a3)==1):
+    print(a3[0])
+else:
+    for i in range(n2-1):
+        print(l3[i],end=' ')
+print(a3[n2-1])
